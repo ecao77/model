781ea9a6c8527be7485d7ada53743bbb9fb2d47f
Enbao Cao <ecao25@sjs.org>
2022-11-24 16:44:06 -0600

    MODEL TIME

--- main.py
+++ main.py
@@ -4,22 +4,20 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-"""
 import json
 import torch
 
 from transformers import BertTokenizer, BertForMaskedLM, BertConfig
 
-output_model_file = "../BERTmodel.bin"
-output_config_file = "../BERTconfig.bin"
-output_vocab_file = "../BERTvocab.bin"
+output_model_file = "./BERTmodel.bin"
+output_config_file = "./BERTconfig.bin"
+output_vocab_file = "./BERTvocab.bin"
 
 config = BertConfig.from_json_file(output_config_file)
 model = BertForMaskedLM(config)
 state_dict = torch.load(output_model_file)
 model.load_state_dict(state_dict)
 tokenizer = BertTokenizer(output_vocab_file)
-"""
 
 app = FastAPI()
 
@@ -38,8 +36,8 @@ def add_item(bodytxt: str):
     return {"id": objects[len(objects) - 1]["id"], "body": objects[len(objects) - 1]["body"]}
     # return {"id": len(objects), "body": bodytxt}
 """
-"""
-@app.get("/predict")
+
+@app.get("/")
 def give_predict(string: str):
     inputs = tokenizer(string, return_tensors="pt")
     with torch.no_grad():
@@ -47,8 +45,9 @@ def give_predict(string: str):
     mask_token_index = (inputs.input_ids == tokenizer.mask_token_id)[0].nonzero(as_tuple=True)[0]
     predicted_token_id = logits[0, mask_token_index].argmax(axis=-1)
     return tokenizer.decode(predicted_token_id)
-"""
 
+"""
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+"""
