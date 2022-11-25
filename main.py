7f5bf162959a7db6d6ae340a199ed9dd971d09ea
Enbao Cao <ecao25@sjs.org>
2022-11-24 19:48:49 -0600

    error?

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ async def give_predict():
         logits = model(**inputs).logits
     mask_token_index = (inputs.input_ids == tokenizer.mask_token_id)[0].nonzero(as_tuple=True)[0]
     predicted_token_id = logits[0, mask_token_index].argmax(axis=-1)
-    return tokenizer.decode(predicted_token_id)
+    return {"message": tokenizer.decode(predicted_token_id)}
 
 """
 @app.get("/")
